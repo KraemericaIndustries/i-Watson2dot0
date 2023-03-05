9b4d37784650283ba1f38a4b38ee89b427162519
Bob Kraemer <bob.kraemer.git@gmail.com>
2023-03-04 23:06:54 -0500

    Add previously unknown words to datafile

--- src/com/kraemericaindustries/ui/Messages.java
+++ src/com/kraemericaindustries/ui/Messages.java
@@ -1,9 +1,14 @@
 package com.kraemericaindustries.ui;
 
 import com.kraemericaindustries.engine.Matrix;
+import com.kraemericaindustries.io.Turn;
 import com.kraemericaindustries.io.Type;
 import com.kraemericaindustries.jdbc.Database;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -65,58 +70,90 @@ public class Messages {
 
         System.out.println("The response was 5!!!");
 
-       do {
-           guessIsOpponentsWord = isGuessOpponentsWord(guess);
+        do {
 
-           if (guessIsOpponentsWord) {
-               System.out.println("Game over man!!!  The opponents word was determined in " + (counter - 1) + " turns.");
-               break;
-           } else {
-               //  DELETE all words that DO NOT contain the letters from the previous guess from the DB...
-               String query = "delete from Words_tbl where word NOT like '%" +
-                       guess.charAt(0) +
-                       "%' or word NOT like '%" +
-                       guess.charAt(1) +
-                       "%' or word NOT like '%" +
-                       guess.charAt(2) +
-                       "%' or word NOT like '%" +
-                       guess.charAt(3) +
-                       "%' or word NOT like '%" +
-                       guess.charAt(4) +
-                       "%'";
-               System.out.println("Deleting ALL words that DO NOT contain the letters in '" + guess + "' from the database...");
-               System.out.println(Database.statement(query) + " rows DELETED from the database.");
 
-               //  COUNT the number of words remaining in the DB...
-               ResultSet resultSet = Database.select("select count (*) from Words_tbl");  //  Execute the statement object
-               //  Process the result
-               while(resultSet.next()) {
-                   count =  ((Number) resultSet.getObject(1)).intValue();
-               }
-               System.out.println(count + " word(s) remaining in the database.");
-               System.out.println();
+//            do {
+                guessIsOpponentsWord = isGuessOpponentsWord(guess);
 
-               // PROCESS of ELIMINATION...
-               System.out.println("In the event the opponent advises that the previous guess (" + guess + ") is NOT their word, all that remains to be done is process of elimination.");
-               System.out.println("Deleting the previous guess (" + guess + ") from the database...");
-               System.out.println(Database.statement("delete from Words_tbl where word = '" + guess + "'") + " row(s) DELETED from the database.");
-               System.out.println();
-               System.out.println("Here are all the OTHER words in the database that can be made from these 5 letters:");
-               try {
-                   resultSet = Database.select("select * from Words_tbl");  //  Execute the statement object
-                   //  Process the result
-                   while(resultSet.next()) {  //  ITERATE over all results returned from the SELECT statement
-                       String word = resultSet.getString("word");  //  SET the value of the local String variable named word to the last value returned by the SQL select statement
-                       System.out.print(word + "\t");
-                   }
-                   System.out.println();
-               } catch (SQLException e) {
-                   e.printStackTrace();
-               }
-               System.out.println("Guess each of the above (in turn) to arrive at the answer.  ");
-               guess = Type.guess(counter);
-           }
-           counter++;
-       } while (!guessIsOpponentsWord);
+                if (guessIsOpponentsWord) {
+                    System.out.println("Game over man!!!  The opponents word was determined in " + (counter - 1) + " turns.");
+                    break;
+                } else {
+                    //  DELETE all words that DO NOT contain the letters from the previous guess from the DB...
+                    String query = "delete from Words_tbl where word NOT like '%" +
+                            guess.charAt(0) +
+                            "%' or word NOT like '%" +
+                            guess.charAt(1) +
+                            "%' or word NOT like '%" +
+                            guess.charAt(2) +
+                            "%' or word NOT like '%" +
+                            guess.charAt(3) +
+                            "%' or word NOT like '%" +
+                            guess.charAt(4) +
+                            "%'";
+                    System.out.println("Deleting ALL words that DO NOT contain the letters in '" + guess + "' from the database...");
+                    System.out.println(Database.statement(query) + " rows DELETED from the database.");
+
+                    //  COUNT the number of words remaining in the DB...
+                    ResultSet resultSet = Database.select("select count (*) from Words_tbl");  //  Execute the statement object
+                    //  Process the result
+                    while(resultSet.next()) {
+                        count =  ((Number) resultSet.getObject(1)).intValue();
+                    }
+                    System.out.println(count + " word(s) remaining in the database.");
+                    System.out.println();
+
+                    // PROCESS of ELIMINATION...
+                    System.out.println("In the event the opponent advises that the previous guess (" + guess + ") is NOT their word, all that remains to be done is process of elimination.");
+                    System.out.println("Deleting the previous guess (" + guess + ") from the database...");
+                    System.out.println(Database.statement("delete from Words_tbl where word = '" + guess + "'") + " row(s) DELETED from the database.");
+
+
+
+                    //  COUNT the number of words remaining in the DB...
+                    resultSet = Database.select("select count (*) from Words_tbl");  //  Execute the statement object
+                    //  Process the result
+                    while(resultSet.next()) {
+                        count =  ((Number) resultSet.getObject(1)).intValue();
+                    }
+
+                    if(count == 0) break;
+
+                    System.out.println();
+                    System.out.println("Here are all the OTHER words in the database that can be made from these 5 letters:");
+                    try {
+                        resultSet = Database.select("select * from Words_tbl");  //  Execute the statement object
+                        //  Process the result
+                        while(resultSet.next()) {  //  ITERATE over all results returned from the SELECT statement
+                            String word = resultSet.getString("word");  //  SET the value of the local String variable named word to the last value returned by the SQL select statement
+                            System.out.print(word + "\t");
+                        }
+                        System.out.println();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("Guess each of the above (in turn) to arrive at the answer.  ");
+                    guess = Type.guess(counter);
+                }
+                counter++;
+//            } while (!guessIsOpponentsWord);
+        } while (count != 0);
+
+        System.out.println("Ya got me!  I'm stumped (this time)!  But I'm adding your word to my database, so the next time I run I KNOW YOUR WORD!  What was your word?:");
+
+
+
+        guess = Turn.enterGuess();
+
+
+
+
+        try {
+            Files.write(Paths.get("C:/Users/bkraemer/OneDrive - Topcon/Development/Java/IntelliJ/i-Watson2dot0/FiveLetterWords.txt"), ("\n" + guess).getBytes(), StandardOpenOption.APPEND);
+        }catch (IOException e) {
+            //exception handling left as an exercise for the reader
+        }
+        System.out.println(" > Added " + guess + " to the data file used to generate the watson database.");
     }
 }
